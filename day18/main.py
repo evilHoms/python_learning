025928d50fe72a3bbf43e9c20dd479a109526d68
evilHoms <homspage@gmail.com>
2023-12-04 19:21:12 +0100

    Day 18 finish

--- day18/main.py
+++ day18/main.py
@@ -1,15 +1,29 @@
 import colorgram
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, colormode
+from random import randint
 
-# colors = colorgram.extract('day18/hirst.jpg', 25)
+GAP = 60
+SIZE = 30
+
+colors = colorgram.extract('day18/hirst.jpg', 25)
 s = Screen()
 t = Turtle()
 t.speed('fastest')
+t.hideturtle()
+colormode(255)
 
 height, width = s.window_height(), s.window_width()
-t.penup()
-t.goto((-100, -100))
-print(height, width)
+x_start = -width / 2 + SIZE
+y_start = height / 2 - SIZE
+x_range = int(width / GAP)
+y_range = int(height / GAP)
+
+for i in range(x_range):
+    for j in range(y_range):
+        t.penup()
+        t.goto((x_start + i * GAP, y_start - j * GAP))
+        rand_color = colors[randint(0, len(colors) - 1)]
+        t.dot(SIZE, rand_color.rgb)
 
 
 s.exitonclick()
